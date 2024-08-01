e13138bc52afe6b0d824adc0095891d2989721e7
Max <knivets.m@gmail.com>
2024-08-01 07:33:15 -0400

    remove the version switcher

--- packages/interface/interface-2.6.4/src/components/Header/index.tsx
+++ packages/interface/interface-2.6.4/src/components/Header/index.tsx
@@ -19,7 +19,7 @@ import Menu from '../Menu'
 
 import Row, { RowBetween } from '../Row'
 import Web3Status from '../Web3Status'
-import VersionSwitch from './VersionSwitch'
+// import VersionSwitch from './VersionSwitch'
 
 const HeaderFrame = styled.div`
   display: flex;
@@ -169,7 +169,6 @@ export default function Header() {
             </AccountElement>
           </HeaderElement>
           <HeaderElementWrap>
-            <VersionSwitch />
             <Settings />
             <Menu />
           </HeaderElementWrap>
